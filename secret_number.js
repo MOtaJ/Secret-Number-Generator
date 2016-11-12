86707ad8fac3acc122942ca2616a80d986d8b55f
Mark Ota <markota@Marks-MacBook-Pro-2.local>
2016-11-12 13:59:59 -1000

    secret number generated

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
+
+
 module.exports = function() {
-  
+    var randomNumber = Math.floor(Math.random() * (1000000 - 0 + 1)) + 0;
+
+    return function generatedNumber(){
+      return randomNumber;
+    };
+
 };
